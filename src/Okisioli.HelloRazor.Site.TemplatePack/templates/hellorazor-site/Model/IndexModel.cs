f028d8e729c175f858dae2a7329e28618b36bab7
Oliver <oliver.g.sk@gmail.com>
2026-07-21 22:33:18 +0200

    Generate site template payload during pack

--- src/Okisioli.HelloRazor.Site.TemplatePack/templates/hellorazor-site/Model/IndexModel.cs
+++
@@ -1,7 +0,0 @@
-using Okisioli.HelloRazor.Core;
-
-namespace Okisioli.HelloRazor.Site.Model;
-
-public class IndexModel() : PageModel("My Awesome Page!")
-{
-}
--- src/Okisioli.HelloRazor.Site.TemplatePack/templates/hellorazor-site/Program.cs
+++
@@ -1,8 +0,0 @@
-using Okisioli.HelloRazor.Core;
-using Okisioli.HelloRazor.Core.Internals;
-using Okisioli.HelloRazor.Site.Model;
-
-var sb = new SiteBuilder()
-    .WithPage(new PageInfo<IndexModel>("Index"));
-
-await sb.Generate();
